4609021274f8b3e3ebd3139e704f91f910dd7ed8
dywoq <aleks.koyf@gmail.com>
2026-01-21 18:36:48 +0300

    base: Add language subpackage

---
+++ base/language/go.mod
@@ -0,0 +1,3 @@
+module github.com/dywoq/vacui/base/language
+
+go 1.25.5
---
+++ base/language/types.go
@@ -0,0 +1,30 @@
+package language
+
+// Debugger represents the standard interface for the debuggers.
+// The print functions work by default, but may behave differently if
+// debugging mode is explicitly enabled in any structure that implements Debugger.
+type Debugger interface {
+	Printf(format string, v ...any)
+	Println(v ...any)
+	Print(v ...any)
+}
+
+// ErrorMaker represents the standard methods of error making.
+// It's purposed for structures that need different error formatting.
+//
+// External developers, relying on ErrorMaker, should write the plain messages only
+// to not conflict with different formatting style of structure that implements ErrorMaker:
+//
+//	func err(e language.ErrorMaker) error {
+//		return e.Error("something failed")
+//	}
+type ErrorMaker interface {
+	Error(msg string) error
+	Errorf(format string, v ...any) error
+}
+
+// Advancer defines standard method for advancing to the next position
+// or something similar.
+type Advancer interface {
+	Advance()
+}
