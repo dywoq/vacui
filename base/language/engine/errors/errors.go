7f08a41844fdc435439d1ae61c870a5d2d95fc83
dywoq <aleks.koyf@gmail.com>
2026-01-22 12:54:37 +0300

    base/language/engine/errors: Adjust documentation and rename ErrorMaker to Maker

--- base/language/engine/errors/errors.go
+++ base/language/engine/errors/errors.go
@@ -1,15 +1,15 @@
 package errors
 
-// ErrorMaker represents the standard methods of error making.
+// Maker represents the standard methods of error making.
 // It's purposed for structures that need different error formatting.
 //
-// External developers, relying on ErrorMaker, should write the plain messages only
-// to not conflict with different formatting style of structure that implements ErrorMaker:
+// External developers, relying on Maker, should write the plain messages only
+// to not conflict with different formatting style of structure that implements Maker:
 //
-//	func err(e language.ErrorMaker) error {
+//	func err(e errors.Maker) error {
 //		return e.Error("something failed")
 //	}
-type ErrorMaker interface {
+type Maker interface {
 	Error(msg string) error
 	Errorf(format string, v ...any) error
 }
