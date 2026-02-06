6c03b1e80c0b1d612506e085c9a8931e67434106
dywoq <aleks.koyf@gmail.com>
2026-02-06 19:13:19 +0300

    cmd: Initialize vacui

---
+++ cmd/vacui/vacui.go
@@ -0,0 +1,13 @@
+// Copyright dywoq 2026
+//
+// This code is published under Apache License 2.0:
+// - http://www.apache.org/licenses/LICENSE-2.0
+//
+// Repository:
+// - https://github.com/dywoq/vacui
+
+package main
+
+func main() {
+
+}
