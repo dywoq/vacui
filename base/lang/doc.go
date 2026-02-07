baa90237b98a029f56da40ee5b29a4a91b04f874
dywoq <aleks.koyf@gmail.com>
2026-02-07 11:32:14 +0300

    base/lang: Create token package and doc.go

---
+++ base/lang/doc.go
@@ -0,0 +1,11 @@
+// Copyright 2026 dywoq
+//
+// This code is released under Apache License 2.0:
+// - https://www.apache.org/licenses/LICENSE-2.0
+//
+// The repository:
+// - https://github.com/dywoq/vacui
+
+// Package lang represents the language engine, which reduces boilerplate code
+// and allows you to create interpreters/compilers faster.
+package lang
