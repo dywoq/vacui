7283dd0c2273737f4d2523b246ef11dc31c7c3c6
dywoq <dywoq.contact@gmail.com>
2026-03-04 07:58:48 +0300

    runtime/service.go: add public function AddService
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ runtime/service.go
@@ -0,0 +1,13 @@
+// Copyright 2026 dywoq - Apache License 2.0
+// Part of Vacui SDK: https://github.com/dywoq/vacui
+
+package runtime
+
+import (
+	"github.com/dywoq/vacui/runtime/internal/subsystem"
+	"github.com/dywoq/vacui/service"
+)
+
+func AddService(s service.Service) error {
+	return subsystem.SCInst.Add(s)
+}
