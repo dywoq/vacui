0941ce94a7737d538576344697c0fc9dfecbb0d3
dywoq <dywoq.contact@gmail.com>
2026-03-10 15:12:59 +0300

    token: Add registers, separators and instructions
    New instructions:
    - push/pop: For managing stack
    
    New keywords/characters: Registers, separators
    
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- token/token.go
+++ token/token.go
@@ -39,6 +39,27 @@ var (
 		"mov",
 		"sys",
 		"ret",
+		"push",
+		"pop",
+	}
+
+	Registers = []string{
+		"x0",
+		"x1",
+		"x2",
+		"x3",
+		"x4",
+		"x5",
+		"x6",
+		"x7",
+	}
+
+	Separators = []string{
+		",",
+		"[",
+		"]",
+		"{",
+		"}",
 	}
 )
 
