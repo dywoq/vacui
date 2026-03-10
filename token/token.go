97e7c36dcfb246dfb678396051e8509415d626fd
dywoq <dywoq.contact@gmail.com>
2026-03-10 18:03:10 +0300

    token: Check if str is instruction, separator, or registry in IsIdentifier
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- token/token.go
+++ token/token.go
@@ -4,7 +4,10 @@
 // Package token represents a token definitions, which is crucial for parser to build AST nodes.
 package token
 
-import "unicode"
+import (
+	"slices"
+	"unicode"
+)
 
 // Kind is used to identify tokens.
 type Kind string
@@ -77,21 +80,24 @@ func New(lit string, kind Kind, pos Pos) *T {
 // If any of these rules are violated, IsIdentifier returns false.
 // Otherwise, it returns true.
 func IsIdentifier(str string) bool {
-    runes := []rune(str)
-    l := len(runes)
-    if l == 0 || l > 255 {
-        return false
-    }
-    for i, r := range runes {
-        if i == 0 && unicode.IsDigit(r) {
-            return false
-        }
-        isLetter := unicode.IsLetter(r)
-        isDigit := unicode.IsDigit(r)
-        isUnderscore := (r == '_')
-        if !isLetter && !isDigit && !isUnderscore {
-            return false
-        }
-    }
-    return true
+	if slices.Contains(Instructions, str) || slices.Contains(Separators, str) || slices.Contains(Registers, str) {
+		return false
+	}
+	runes := []rune(str)
+	l := len(runes)
+	if l == 0 || l > 255 {
+		return false
+	}
+	for i, r := range runes {
+		if i == 0 && unicode.IsDigit(r) {
+			return false
+		}
+		isLetter := unicode.IsLetter(r)
+		isDigit := unicode.IsDigit(r)
+		isUnderscore := (r == '_')
+		if !isLetter && !isDigit && !isUnderscore {
+			return false
+		}
+	}
+	return true
 }
