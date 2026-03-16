cc37f5a4d19e085da17551b1d78fec57f2315d39
dywoq <dywoq.contact@gmail.com>
2026-03-16 21:41:21 +0300

    kernel/hub: panic.c: use hub_printnf instead of intenral print_
    Since we have hub_printnf right now, it's meaningless to use print_

--- kernel/hub/panic.c
+++ kernel/hub/panic.c
@@ -2,24 +2,13 @@
 // A part of https://github.com/dywoq/zero
 
 #include <bios/vid.h>
-
-// Will be replaced when we're going to have normal print function
-static void print_(const char *msg) {
-    for (const char *p = msg; *p != '\0'; ++p) {
-        if (*p == '\n') {
-            bios_vid_tt_output('\r');
-            bios_vid_tt_output('\n');
-            continue;
-        }
-        bios_vid_tt_output(*p);
-    }
-}
+#include <hub/print.h>
 
 void hub_panic(const char *msg) {
     bios_vid_mode_set(BIOS_VID_TEXT_X80_25);
-    print_("--------------- Zero panic ----------------\n");
-    print_(msg);
-    print_("\n-------------------------------------------");
+    hub_printnf("--------------- Zero panic ----------------\n");
+    hub_printnf(msg);
+    hub_printnf("\n-------------------------------------------");
     while (true) {
         __asm volatile("hlt\n");
     }
