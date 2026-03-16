69b427996f4ac261d7950478552d4cf6ba08994d
dywoq <dywoq.contact@gmail.com>
2026-03-16 17:04:20 +0300

    include/vga: Remove vga module

--- include/std/types.h
+++ include/std/types.h
@@ -8,16 +8,16 @@
 //
 // Shortcut types; Typedefs around integers
 
-typedef signed char std_byte_t;
-typedef unsigned char std_ubyte_t;
+typedef signed char byte_t;
+typedef unsigned char ubyte_t;
 
-typedef signed short std_word_t;
-typedef unsigned short std_uword_t;
+typedef signed short word_t;
+typedef unsigned short uword_t;
 
-typedef signed int std_dword_t;
-typedef unsigned int std_udword_t;
+typedef signed int dword_t;
+typedef unsigned int udword_t;
 
-typedef signed long long std_qword_t;
-typedef unsigned long long std_uqword_t;
+typedef signed long long qword_t;
+typedef unsigned long long uqword_t;
 
 #endif
--- kernel/hub/hub.c
+++ kernel/hub/hub.c
@@ -2,10 +2,8 @@
 // A part of https://github.com/dywoq/zero
 
 #include <std/types.h>
-#include <vga/output.h>
 
 void hub() {
-    vga_output_str("Hi!");
     while (true) {
         __asm volatile("hlt\n");
     }
