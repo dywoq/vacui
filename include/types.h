fac5f9bc733e97dd1819b5dd19ed206ca208b559
dywoq <dywoq.contact@gmail.com>
2026-03-16 14:30:23 +0300

    include: add types.h header

---
+++ include/types.h
@@ -0,0 +1,23 @@
+// Copyright 2026 dywoq - Apache License 2.0
+// A part of https://github.com/dywoq/zero
+
+#ifndef _TYPES_H
+#define _TYPES_H
+
+// Header overview
+//
+// Shortcut types; Typedefs around integers
+
+typedef signed char byte_t;
+typedef unsigned char ubyte_t;
+
+typedef signed short word_t;
+typedef unsigned short uword_t;
+
+typedef signed int dword_t;
+typedef unsigned int udword_t;
+
+typedef signed long long qword_t;
+typedef unsigned long long uqword_t;
+
+#endif
