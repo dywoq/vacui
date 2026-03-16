2366ae3ad80d8eb03836d77bd6d5aca50d5be211
dywoq <dywoq.contact@gmail.com>
2026-03-16 14:34:55 +0300

    kernel: Initialize std module

---
+++ include/std/types.h
@@ -0,0 +1,23 @@
+// Copyright 2026 dywoq - Apache License 2.0
+// A part of https://github.com/dywoq/zero
+
+#ifndef _STD_TYPES_H
+#define _STD_TYPES_H
+
+// Header overview
+//
+// Shortcut types; Typedefs around integers
+
+typedef signed char std_byte_t;
+typedef unsigned char std_ubyte_t;
+
+typedef signed short std_word_t;
+typedef unsigned short std_uword_t;
+
+typedef signed int std_dword_t;
+typedef unsigned int std_udword_t;
+
+typedef signed long long std_qword_t;
+typedef unsigned long long std_uqword_t;
+
+#endif
--- kernel/hub/hub.c
+++ kernel/hub/hub.c
@@ -1,7 +1,10 @@
 // Copyright 2026 dywoq - Apache License 2.0
 // A part of https://github.com/dywoq/zero
 
+#include <types.h>
+
 void hub() {
     while (true) {
+        byte_t d = 2;
     }
 }
