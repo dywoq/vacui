c1c7147e6ee721733b2c23f34c3a08137d677da3
dywoq <dywoq.contact@gmail.com>
2026-03-18 21:51:34 +0300

    std/types.h: rename types
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- include/std/types.h
+++ include/std/types.h
@@ -14,10 +14,10 @@ typedef unsigned char ubyte_t;
 typedef signed short word_t;
 typedef unsigned short uword_t;
 
-typedef signed int dword_t;
-typedef unsigned int udword_t;
+typedef signed int long_t;
+typedef unsigned int ulong_t;
 
-typedef signed long long qword_t;
-typedef unsigned long long uqword_t;
+typedef signed long long quad_t;
+typedef unsigned long long uquad_t;
 
 #endif
