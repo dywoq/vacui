d010b4c08baec682d6e37b0d38cc8c6aea2a3383
dywoq <dywoq.contact@gmail.com>
2026-03-22 10:52:37 +0300

    Delete files
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- include/hub/panic.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2026 dywoq - Apache License 2.0
-// A part of https://github.com/dywoq/zero
-
-#ifndef _HUB_PANIC_H
-#define _HUB_PANIC_H
-
-[[noreturn]] void hub_panic(const char *msg);
-
-#endif
--- include/std/def.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2026 dywoq - Apache License 2.0
-// A part of https://github.com/dywoq/zero
-
-#ifndef _STD_DEF_H
-#define _STD_DEF_H
-
-#define STR(x) #x
-
-#endif
