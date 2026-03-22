9950f7a8fd40fedf1d5e4fa13187220e2a9a1d9b
dywoq <dywoq.contact@gmail.com>
2026-03-22 20:07:19 +0300

    Init boot/load project
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- boot/load/hub/hub.c
+++ boot/load/hub/hub.c
@@ -1,7 +1,10 @@
 // Copyright 2026 dywoq - Apache License 2.0
 // A part of https://github.com/dywoq/zero
 
+#include <zero/boot/load/autoconf.h>
+
 void hub ()
 {
+        while (true) {
+        }
 }
-
