cca84e1a4d14962929634e2b12c6de8cc30475c2
dywoq <dywoq.contact@gmail.com>
2026-03-23 19:18:28 +0300

    include/zero/types.h: remove prefix 'zr_'
    Since it's internal and public only to kernel/OS parts,
    it's unnecessary to have 'zr_' prefix.
    
    Removing it will improve readability.
    
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- include/zero/types.h
+++ include/zero/types.h
@@ -4,16 +4,18 @@
 #ifndef _ZERO_TYPES_H
 #define _ZEOR_TYPES_H
 
-typedef signed char zr_byte_t;
-typedef unsigned char zr_ubyte_t;
+typedef signed char byte_t;
+typedef unsigned char ubyte_t;
 
-typedef signed short zr_word_t;
-typedef unsigned short zr_uword_t;
+typedef signed short word_t;
+typedef unsigned short uword_t;
 
-typedef signed int zr_dword_t;
-typedef unsigned int zr_udword_t;
+typedef signed int dword_t;
+typedef unsigned int udword_t;
 
-typedef signed long int zr_qword_t;
-typedef unsigned long int zr_uqword_t;
+typedef signed long long int qword_t;
+typedef unsigned long long int uqword_t;
+
+typedef uword_t usize_t;
 
 #endif
