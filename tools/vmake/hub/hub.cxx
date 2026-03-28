29e11c78e3579140af611bf142e418ab647ff9ac
dywoq <dywoq.contact@gmail.com>
2026-03-28 21:02:43 +0300

    Push March 28 2026 changes
    Changelog:
    - vmake: tools/vmake/changelog/2026/march/28.txt
    
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ tools/vmake/hub/hub.cxx
@@ -0,0 +1,28 @@
+/**
+	Copyright 2026 dywoq - Apache License 2.0
+	A part of github.com/dywoq/vacui
+
+	Module name:
+		hub.cxx
+
+	Module description:
+		Represents an entry file of vmake
+
+	Revisions:
+		28-Mar 2026
+		* Creation date
+
+	Authors:
+		dywoq github.com/dywoq dywoq.contact@gmail.com
+**/
+
+#include <iostream>
+
+int
+main()
+{
+	std::cout << "vmake: Build configuration tool, specifically made for Vacui" << std::endl;
+	std::cout << "	Project: github.com/dywoq/vacui" << std::endl;
+	std::cout << "	Copyright 2026 dywoq - Apache License 2.0" << std::endl;
+	return 0;
+}
