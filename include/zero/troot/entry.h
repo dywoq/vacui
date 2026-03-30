05679756882c4b08fcb6db2f2eccb3df29d4d72c
dywoq <dywoq.contact@gmail.com>
2026-03-30 12:18:10 +0300

    Remove
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- include/zero/troot/entry.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2026 dywoq - Apache License 2.0
-// A part of https://github.com/dywoq/zero
-
-#ifndef _ZERO_TROOT_ENTRY_H
-#define _ZERO_TROOT_ENTRY_H
-
-[[noreturn]] void troot_entry ();
-
-#endif
--- tools/zero_cli/src/main.rs
+++
@@ -1,6 +0,0 @@
-// Copyright 2026 dywoq - Apache License 2.0
-// A part of https://github.com/dywoq/zero 
-
-fn main() { 
-    println!("Hello, world!");
-}
