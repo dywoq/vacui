3a19ac5c9756d02c0be844b17806fb3e5d6ebc7a
dywoq <dywoq.contact@gmail.com>
2026-03-30 20:28:12 +0300

    boot/load: Init bios module
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- boot/load/entry/entry.c
+++ boot/load/entry/entry.c
@@ -1,12 +1,16 @@
 // Copyright 2026 dywoq - Apache License 2.0
 // A part of https://github.com/dywoq/zero
 
+__asm(".code16");
+
+#include <bios/video.h>
 #include <zero/types.h>
 
 void entry()
 {
+    bios_video_mode_set(0x13);
     while (true)
     {
-        __asm volatile("hlt\n");
+        __asm volatile("hlt\n" : : :);
     }
 }
