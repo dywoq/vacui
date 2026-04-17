9940f25eae3c8c361335dda6bb933e58b0730580
dywoq <dywoq.contact@gmail.com>
2026-04-17 15:59:59 +0300

    change naming style and remove some parts
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- boot/load/hub/hub.c
+++ boot/load/hub/hub.c
@@ -1,14 +1,10 @@
 // Copyright 2026 dywoq - Apache License 2.0
 // A part of https://github.com/dywoq/vacui
 
-#include <vacui/boot/bios/vid.h>
-
 __asm(".code16gcc");
 
-void
-Hub()
+void hub()
 {
-    BiosSwitchVidMode(0x13);
     while (true)
         __asm volatile("hlt");
 }
