6243cb01bf4f5d5eec22846fa27da82bb24aef2a
dywoq <dywoq.contact@gmail.com>
2026-04-17 17:56:04 +0300

    include/vacui/types.h: add types.h header
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ include/vacui/types.h
@@ -0,0 +1,20 @@
+// Copyright 2026 dywoq - Apache License 2.0
+// A part of https://github.com/dywoq/vacui
+
+#ifndef _VACUI_TYPES_H
+#define _VACUI_TYPES_H
+
+typedef signed char vac_byte_t;
+typedef unsigned char vac_ubyte_t;
+
+typedef signed short vac_word_t;
+typedef unsigned short vac_uword_t;
+
+typedef signed long int vac_long_t;
+typedef unsigned long int vac_ulong_t;
+
+typedef signed long long vac_qword_t;
+typedef unsigned long long vac_uqword_t;
+
+#endif
+
