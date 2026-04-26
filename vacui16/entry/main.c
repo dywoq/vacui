0386cafdaedbf695fa156902588fe21b33038c21
dywoq <dywoq.contact@gmail.com>
2026-04-26 09:54:49 +0300

    entry/main.c: add missing module description
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- vacui16/entry/main.c
+++ vacui16/entry/main.c
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2026 dywoq - Apache License 2.0
+ * A part of https://github.com/dywoq/vacui
+ *
+ * Module name:
+ * 	entry/main.c
+ *
+ * Module description:
+ * 	A kernel entry file
+ *
+ * History:
+ * 	Apr-25 10:12PM 2026
+ * 	- Creation date
+ *
+ * Authors:
+ * 	dywoq dywoq.contact@gmail.com
+ */
+	
 void 
 Entry(
 	void) 
