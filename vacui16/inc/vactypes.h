ff637b278aab29916176e374506ab5219227f8ef
dywoq <dywoq.contact@gmail.com>
2026-04-26 09:45:31 +0300

    vactypes.h: add integral types and void * wrapper
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ vacui16/inc/vactypes.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2026 dywoq - Apache License 2.0
+ * A part of https://github.com/dywoq/vacui
+ *
+ * Module name:
+ * 	vactypes.h
+ *
+ * Module description:
+ * 	Shortcut types, integral types
+ *
+ * History:
+ * 	Apr-26 9:44 AM 2026
+ * 	- Creation date
+ *
+ * Authors:
+ * 	dywoq dywoq.contact@gmail.com
+ */
+#ifndef _VACTYPES_H
+#define _VACTYPES_H
+
+typedef char 			VAC_CHAR;
+typedef short			VAC_SHORT;
+typedef int			VAC_INT;
+typedef long long 		VAC_LONG;
+
+typedef unsigned char 		VAC_UCHAR;
+typedef unsigned short 		VAC_USHORT;
+typedef unsigned int 		VAC_UINT;
+typedef unsigned long long 	VAC_ULONG;
+
+typedef void *			VAC_PVOID;
+
+#endif
