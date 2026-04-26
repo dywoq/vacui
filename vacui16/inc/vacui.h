f0a8fd510dc9feec186bca32a04bf1e83d5e614e
dywoq <dywoq.contact@gmail.com>
2026-04-26 10:03:25 +0300

    vacui.h: include vacdef.h and vactypes.h
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ vacui16/inc/vacui.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2026 dywoq - Apache License 2.0
+ * A part of https://github.com/dywoq/vacui
+ *
+ * Module name:
+ * 	vacui.h
+ *
+ * Module description:
+ * 	Master header
+ *
+ * History:
+ * 	Apr-26 10:03 AM 2026
+ * 	- Creation date
+ *
+ * Authors:
+ * 	dywoq dywoq.contact@gmail.com
+ */
+#ifndef _VACUI_H
+#define _VACUI_H
+
+#include <vactypes.h>
+#include <vacdef.h>
+
+#endif
