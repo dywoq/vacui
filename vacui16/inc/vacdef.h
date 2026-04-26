5f3369550d2e56d7bd33ffe0c4ed49973dcc2709
dywoq <dywoq.contact@gmail.com>
2026-04-26 09:50:48 +0300

    vacdef.h: add documentation-only macros
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ vacui16/inc/vacdef.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2026 dywoq - Apache License 2.0
+ * A part of https://github.com/dywoq/vacui
+ *
+ * Module name:
+ * 	vacdef.h
+ *
+ * Module description:
+ * 	Definition macros
+ *
+ * History:
+ * 	Apr-26 9:50 AM 2026
+ * 	- Creation date
+ *
+ * Authors:
+ * 	dywoq dywoq.contact@gmail.com
+ */
+#ifndef _VACDEF_H
+#define _VACDEF_H
+
+/* Documentation-only macros */
+#ifndef IN
+#define IN
+#endif
+#ifndef OUT
+#define OUT
+#endif
+#ifndef OPT
+#define OPT
+#endif
+
+#endif
