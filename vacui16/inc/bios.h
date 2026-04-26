f1e3efdb22457de6ef7902fe4762a4599211c3db
dywoq <dywoq.contact@gmail.com>
2026-04-26 10:21:20 +0300

    Create bios.h
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ vacui16/inc/bios.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2026 dywoq - Apache License 2.0
+ * A part of https://github.com/dywoq/vacui
+ *
+ * Module name:
+ * 	bios.h
+ *
+ * Module description:
+ * 	Master header of BIOS
+ *
+ * History:
+ * 	Apr-26 10:03 AM 2026
+ * 	- Creation date
+ *
+ * Authors:
+ * 	dywoq dywoq.contact@gmail.com
+ */
+#ifndef _BIOS_H
+#define _BIOS_H
+
+
+
+#endif
