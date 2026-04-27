ca59e95c5f20b02e155d132dbaef0cb2f479b4e2
dywoq <dywoq.contact@gmail.com>
2026-04-27 21:13:50 +0300

    vqtypes.h: add shortcut types and VOID/PVOID
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ vacmini/inc/vqtypes.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2026 dywoq - Apache License 2.0
+ * A part of https://github.com/dywoq/vacui
+ *
+ * Module name:
+ * 	vqtypes.h
+ *
+ * Module description:
+ * 	Shortcut types; Integral type aliases 
+ *
+ * Authors:
+ * 	dywoq dywoq.contact@gmail.com
+ */
+
+#ifndef _VQTYPES_H
+#define _VQTYPES_H
+
+typedef char		BYTE;
+typedef short		SHORT;
+typedef int		INT;
+typedef long long	LONG;
+
+typedef unsigned BYTE 	UBYTE;
+typedef unsigned SHORT	USHORT;
+typedef unsigned INT	UINT;
+typedef unsigned LONG	ULONG;
+
+typedef void		VOID;
+typedef VOID*		PVOID;
+
+#endif
