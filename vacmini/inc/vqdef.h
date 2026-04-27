3850282a9ef54791f1ace6adb22f6486e0d4bc59
dywoq <dywoq.contact@gmail.com>
2026-04-27 20:26:08 +0300

    vqdef.h: add NULL and docs macros
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ vacmini/inc/vqdef.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2026 dywoq - Apache License 2.0
+ * A part of https://github.com/dywoq/vacui
+ *
+ * Module name:
+ * 	vqdef.h
+ *
+ * Description:
+ * 	The macro definitions
+ *
+ * Authors:
+ * 	dywoq dywoq.contact@gmail.com
+ */
+
+#ifndef _VQDEF_H
+#define _VQDEF_H
+
+/* Null macro definition */
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
+
+/* Documentation only macros */
+#ifndef IN
+#define IN
+#endif
+#ifndef OUT
+#define OUT
+#endif
+#ifndef OPT
+#define OPT
+#endif
+
+#endif
