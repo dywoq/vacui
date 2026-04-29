77e2befc7ba59ce48879acc00d3521b11cd01e12
dywoq <dywoq.contact@gmail.com>
2026-04-29 20:40:49 +0300

    vqdocdef.h: add doc-specific functions
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ vacmini/include/vqdocdef.h
@@ -0,0 +1,20 @@
+// Copyright 2026 dywoq - Apache License 2.0
+// A part of https://github.com/dywoq/vacui
+//
+// Module description:
+// - The documentation-specific macros.
+
+#ifndef _VQDOCDEF_H
+#define _VQDOCDEF_H
+
+#ifndef IN
+#define IN
+#endif
+#ifndef OUT
+#define OUT
+#endif
+#ifndef OPT
+#define OPT
+#endif
+
+#endif
