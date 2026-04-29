82936df79357ebc209ac1fc58ac6f506a0d4d0cb
dywoq <dywoq.contact@gmail.com>
2026-04-29 19:03:43 +0300

    vqtypes.h: init; create primitive type aliases

---
+++ vacmini/include/vqtypes.h
@@ -0,0 +1,21 @@
+// Copyright 2026 dywoq - Apache License 2.0
+// A part of https://github.com/dywoq/vacui
+//
+// Module description:
+// - The unified and short type aliases of primitive types.
+
+#ifndef _VQTYPES_H
+#define _VQTYPES_H
+
+// Primitive types
+typedef char               vq_i8_t;
+typedef unsigned char      vq_u8_t;
+typedef short              vq_i16_t;
+typedef unsigned short     vq_u16_t;
+typedef int                vq_i32_t;
+typedef unsigned int       vq_u32_t;
+typedef long long          vq_i64_t;
+typedef unsigned long long vq_u64_t;
+
+#endif
+ 
