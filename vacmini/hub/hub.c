1104e766df3be2384bfdfb438c17f44017dc0f6b
dywoq <dywoq.contact@gmail.com>
2026-04-29 20:09:41 +0300

    hub/hub.c: init hub() kernel function

---
+++ vacmini/hub/hub.c
@@ -0,0 +1,12 @@
+// Copyright 2026 dywoq - Apache License 2.0
+// A part of https://github.com/dywoq/vacui
+//
+// Module description:
+// - The unified and short type aliases of primitive types.
+
+__asm(".code16gcc");
+
+[[noreturn]] void hub() {
+    while (true)
+        ;
+}
