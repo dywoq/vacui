c6122578c60e46e5014c2e49451e040ef1aae0b8
dywoq <dywoq.contact@gmail.com>
2026-04-30 20:16:17 +0300

    scripts/build.sh: update building of .img file

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/bash
 
-cd boot && make clean all && cd ../
-xmake
-
-objcopy -O binary .build/linux/x86_64/release/kernel .build/kernel.bin
-
 dd if=/dev/zero of=vacui.img bs=1M count=10
-dd if=boot/.build/sector.bin of=vacui.img conv=notrunc count=1
-dd if=.build/kernel.bin of=vacui.img conv=notrunc seek=1
+
+# Build boot sector
+cd boot/sector && make clean all && cd ../../
+dd if=boot/sector/.build/vqboot_sector.bin of=vacui.img conv=notrunc count=1
+
+# Build boot primary stage
+xmake build vqboot_primary
+objcopy -O binary .build/linux/x86_64/release/vqboot_primary .build/vqboot_primary.bin
+dd if=.build/vqboot_primary.bin of=vacui.img conv=notrunc seek=1
+
+# Build kernel
+xmake build vqkrnl
+objcopy -O binary .build/linux/x86_64/release/vqkrnl .build/vqkrnl.bin
+dd if=.build/vqkrnl.bin of=vacui.img conv=notrunc seek=2
