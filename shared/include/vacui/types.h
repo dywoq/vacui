4bef2fee3f7117e7234405b6fc7775d309c89299
dywoq <dywoq.contact@gmail.com>
2026-04-30 20:57:35 +0300

    shared/include: add types.h
    The vacui/types.h header contains a short type aliases over primitive
    types (and not only in future).
    
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ shared/include/vacui/types.h
@@ -0,0 +1,20 @@
+// Copyright 2026 dywoq - Apache License 2.0
+// A part of https://github.com/dywoq/vacui
+//
+// Description:
+//
+// Shortcut type aliases over primitive types
+
+#ifndef _VACUI_TYPES_H
+#define _VACUI_TYPES_H
+
+typedef char               vac_byte_t;
+typedef short              vac_short_t;
+typedef int                vac_int_t;
+typedef long long          vac_long_t;
+typedef unsigned char      vac_ubyte_t;
+typedef unsigned short     vac_ushort_t;
+typedef unsigned int       vac_uint_t;
+typedef unsigned long long vac_ulong_t;
+
+#endif
