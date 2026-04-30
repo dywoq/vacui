df831758d70f135f9365083cebb0ecd7304addc4
dywoq <dywoq.contact@gmail.com>
2026-04-30 20:51:57 +0300

    shared/include: add docdef.h header
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ shared/include/vacui/docdef.h
@@ -0,0 +1,21 @@
+// Copyright 2026 dywoq - Apache License 2.0
+// A part of https://github.com/dywoq/vacui
+//
+// Description:
+//
+// The self documentation-purpose macros
+
+#ifndef _VACUI_DOCDEF_H
+#define _VACUI_DOCDEF_H
+
+#ifndef IN
+#define IN
+#endif
+#ifndef OUT
+#define OUT
+#endif
+#ifndef OPT
+#define OPT
+#endif
+
+#endif
