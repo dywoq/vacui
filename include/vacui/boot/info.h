9edde7a41503909279270b04b401bee70ac7c743
dywoq <dywoq.contact@gmail.com>
2026-05-01 10:36:45 +0300

    info.h: create time info declaration, and unified vq_boot_info
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ include/vacui/boot/info.h
@@ -0,0 +1,25 @@
+// Copyright 2026 dywoq - Apache License 2.0
+// A part of https://github.com/dywoq/vacui
+//
+// Description:
+//
+// A header with bootloader information declarations.
+// They are passed to the kernel from bootloader primary stage.
+
+#ifndef _VACUI_BOOT_INFO_H
+#define _VACUI_BOOT_INFO_H
+
+#include <vacui/types.h>
+
+typedef struct vq_boot_info_time {
+    vac_ubyte_t hours;
+    vac_ubyte_t minutes;
+    vac_ubyte_t seconds;
+    vac_ubyte_t daylight_savings_flag;
+} vq_boot_info_time_t;
+
+typedef struct vq_boot_info {
+    vq_boot_info_time_t *time;
+} vq_boot_info_t;
+
+#endif
