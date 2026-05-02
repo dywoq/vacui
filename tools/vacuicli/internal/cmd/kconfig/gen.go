96c3ead68fb35af386cf2f24ff5aac1639c757bd
dywoq <dywoq.contact@gmail.com>
2026-05-02 09:44:56 +0300

    internal/cmd/kconfig: fix relative path bug
    The genconfig outputs an error if it cannot find Kconfig file
    "debug/Kconfig", because it tries to find it in relative path.
    
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- tools/vacuicli/internal/cmd/kconfig/gen.go
+++ tools/vacuicli/internal/cmd/kconfig/gen.go
@@ -25,36 +25,48 @@ var genModules = []genModule{
 }
 
 func Gen() *cobra.Command {
-	gen := &cobra.Command{
+	return &cobra.Command{
 		Use:   "gen",
 		Short: "Generate KConfig header files",
-		Run: func(cmd *cobra.Command, args []string) {
-			for _, genModule := range genModules {
-				headerFolder := filepath.Dir(genModule.headerPath)
+		RunE: func(cmd *cobra.Command, args []string) error {
+			for _, gm := range genModules {
+				moduleDir := filepath.Dir(gm.kconfig)
 
-				if _, err := os.Stat(genModule.config); errors.Is(err, os.ErrNotExist) {
-					fmt.Fprintf(os.Stderr, "error: Config %s file doesn't exist\n", genModule.config)
-					os.Exit(1)
-				}
-				
-				if err := os.MkdirAll(headerFolder, os.ModePerm); err != nil {
-					fmt.Fprintf(os.Stderr, "error: Failed to create directory %s: %v\n", headerFolder, err)
-					os.Exit(1)
+				if _, err := os.Stat(gm.config); errors.Is(err, os.ErrNotExist) {
+					return fmt.Errorf("config %s file doesn't exist", gm.config)
+				} else if err != nil {
+					return err
 				}
 
-				cmd := exec.Command("genconfig", genModule.kconfig, "--header-path", genModule.headerPath)
+				if err := os.MkdirAll(filepath.Dir(gm.headerPath), os.ModePerm); err != nil {
+					return fmt.Errorf("failed to create directory %s: %w", filepath.Dir(gm.headerPath), err)
+				}
 
-				cmd.Env = append(os.Environ(), fmt.Sprintf("KCONFIG_CONFIG=%s", genModule.config))
-
-				bytes, err := cmd.CombinedOutput()
+				configRel, err := filepath.Rel(moduleDir, gm.config)
 				if err != nil {
-					fmt.Fprintf(os.Stderr, "error: Failed to generate header file %s:\n%s\n", genModule.headerPath, string(bytes))
-					os.Exit(1)
+					return fmt.Errorf("failed to make config path relative: %w", err)
 				}
 
-				fmt.Printf("Generated %s\n", genModule.headerPath)
+				headerRel, err := filepath.Rel(moduleDir, gm.headerPath)
+				if err != nil {
+					return fmt.Errorf("failed to make header path relative: %w", err)
+				}
+
+				kconfigFile := filepath.Base(gm.kconfig)
+
+				genCmd := exec.Command("genconfig", kconfigFile, "--header-path", headerRel)
+				genCmd.Dir = moduleDir
+				genCmd.Env = append(os.Environ(), fmt.Sprintf("KCONFIG_CONFIG=%s", configRel))
+
+				output, err := genCmd.CombinedOutput()
+				if err != nil {
+					return fmt.Errorf("failed to generate header file %s:\n%s", gm.headerPath, string(output))
+				}
+
+				fmt.Printf("Generated %s\n", gm.headerPath)
 			}
+
+			return nil
 		},
 	}
-	return gen
 }
