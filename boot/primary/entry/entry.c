d82a5ec957ee923354a8a425ceba426eae78aa80
dywoq <dywoq.contact@gmail.com>
2026-05-02 14:05:47 +0300

    entry/entry.c: add warning about debug build
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- boot/primary/entry/entry.c
+++ boot/primary/entry/entry.c
@@ -7,11 +7,14 @@
 
 __asm(".code16gcc");
 
+#include <debug/debug.h>
+
 #include <base/print.h>
 
 [[noreturn]] void entry()
 {
     base_print_raw("| Boot Primary stage is started\n\r");
+    debug_raw("NON-OPTIMIZED BUILD OF 'vqboot_primary'\n\r");   
     while (true)
         __asm volatile("hlt");
 }
