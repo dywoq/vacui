48dbf8ff01178bb83885a104618d93f271b93b52
dywoq <dywoq.contact@gmail.com>
2026-05-09 21:57:31 +0300

    bootinfo.h: add boot_kernel_mode enum and its field in boot_info
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ include/vacui/bootinfo.h
@@ -0,0 +1,16 @@
+// Copyright dywoq 2026 - Apache License 2.0
+// https://github.com/dywoq/vacui
+
+#ifndef _VACUI_BOOTINFO_H
+#define _VACUI_BOOTINFO_H
+
+enum boot_kernel_mode : unsigned char {
+  BOOT_KERNEL_RECOVERY,
+  BOOT_KERNEL_NORMAL,
+};
+
+struct boot_info {
+  enum boot_kernel_mode kernel_mode;
+};
+
+#endif
