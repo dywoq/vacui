66d68f0b3461db7a1e57f8f1d5056fafd49cef28
dywoq <dywoq.contact@gmail.com>
2026-05-09 15:18:07 +0300

    Add ld.h header
    The ld.h header contains thin macro-wrappers around GCC-compiler
    attributes. Currently, it has __bios, which puts symbol into .bios
    section (added into linker.ld).
    
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ boot/primary/include/vacui/primary/ld.h
@@ -0,0 +1,9 @@
+// Copyright dywoq 2026 - Apache License 2.0
+// https://github.com/dywoq/vacui
+
+#ifndef _VACUI_PRIMARY_LD_H
+#define _VACUI_PRIMARY_LD_H
+
+#define __bios [[gnu::section(".bios")]]
+
+#endif
