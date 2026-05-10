9f76586146a9b48e2b3db08982ec5802660c9e2e
dywoq <dywoq.contact@gmail.com>
2026-05-10 10:20:10 +0300

    hub/main.c: ask for kernel mode
    Changes:
    - New ask_for_kernel_mode_ function. It blocks CPU execution and asks
      for kernel mode. The inputs:
       - 1 -> Normal mode
       - 2 -> Recovery mode
         Writes kernel mode to boot_info_ struct instance.
    - New instance of boot_info_ structure.
    - Call ask_for_kernel_mode_ in primary() function.
    
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- boot/primary/hub/main.c
+++ boot/primary/hub/main.c
@@ -1,6 +1,7 @@
 // Copyright dywoq 2026 - Apache License 2.0
 // https://github.com/dywoq/vacui
 
+#include <vacui/bootinfo.h>
 #include <vacui/primary/bios.h>
 #include <vacui/primary/hub.h>
 
@@ -16,6 +17,7 @@ struct bios_dpa kernel_dap_ = {
 };
 
 struct bios_vbe_info vbe_info_ = {};
+struct boot_info boot_info_ = {};
 
 static void init_vbe_info_()
 {
@@ -36,10 +38,32 @@ static void load_kernel_dap_()
         hub_puts("Loaded kernel with its Disk Address Packet\n\r");
 }
 
+static void ask_for_kernel_mode_()
+{
+        char scan_code, ascii_char = 0;
+        hub_puts("Choose kernel mode (1 = Normal, 2 = Recovery): ");
+
+repeat:
+        bios_get_keystroke(&scan_code, &ascii_char);
+        switch (ascii_char) {
+        case '1':
+                hub_puts("1\n\r");
+                boot_info_.kernel_mode = BOOT_KERNEL_NORMAL;
+                return;
+        case '2':
+                hub_puts("2\n\r");
+                boot_info_.kernel_mode = BOOT_KERNEL_RECOVERY;
+                return;
+        default:
+                goto repeat;
+        }
+}
+
 void primary()
 {
         init_vbe_info_();
         load_kernel_dap_();
+        ask_for_kernel_mode_();
 
         while (1)
                 __asm volatile("hlt\n");
