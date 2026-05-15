d420d94ce9e73b17e3d05c102785a42ee9753faf
dywoq <dywoq.contact@gmail.com>
2026-05-15 10:05:11 +0300

    create vqtypes.h
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ include/vqtypes.h
@@ -0,0 +1,19 @@
+/*
+Copyright 2026 dywoq - Apache License 2.0
+https://github.com/dywoq/vacui
+*/
+
+#ifndef _VQTYPES_H
+#define _VQTYPES_H
+
+typedef char byte_t;
+typedef short short_t;
+typedef int int_t;
+typedef long long long_t;
+
+typedef unsigned char ubyte_t;
+typedef unsigned short ushort_t;
+typedef unsigned int uint_t;
+typedef unsigned long long ulong_t;
+
+#endif
