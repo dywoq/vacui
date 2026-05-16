04d570926e9e96ade7a2e4a2d45d364626831652
dywoq <dywoq.contact@gmail.com>
2026-05-16 12:54:16 +0300

    Create vqdef.h
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ base/inc/vqdef.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2026 dywoq - Apache License 2.0
+ * https://github.com/dywoq/vacui
+ *
+ * This header contains the definition, shortcut types.
+ * A part of shared library.
+ */
+
+#ifndef _VQDEF_H
+#define _VQDEF_H
+
+typedef char byte_t;
+typedef short short_t;
+typedef int int_t;
+typedef long long long_t;
+
+typedef unsigned char ubyte_t;
+typedef unsigned short ushort_t;
+typedef unsigned int uint_t;
+typedef unsigned long long ulong_t;
+
+typedef uint_t usize_t;
+
+#endif
