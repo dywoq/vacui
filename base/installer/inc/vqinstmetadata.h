9329d6d89555a2ff14c4cb3c343af9fc763d5acf
dywoq <dywoq.contact@gmail.com>
2026-05-17 22:41:13 +0300

    vqinstmetadata.h: document api
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- base/installer/inc/vqinstmetadata.h
+++ base/installer/inc/vqinstmetadata.h
@@ -9,14 +9,25 @@
 #ifndef _VQINSTMETADATA_H
 #define _VQINSTMETADATA_H
 
+/*
+ * Provides  the installer title in string.
+ */
 [[nodiscard]]
 const char *
 MetadataGetTitle();
 
+/*
+ * Provides the build type in string. Return values:
+ * - If macro RELEASE is defined -> "Release build"
+ * - If macro DEBUG is defined -> "Debug build"
+ */
 [[nodiscard]]
 const char *
 MetadataGetBuildType();
 
+/*
+ * Provides the installer version in string.
+ */
 [[nodiscard]]
 const char *
 MetadataGetVersion();
