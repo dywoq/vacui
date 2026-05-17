fd9c5f70100280a7e0208184ee38b131e3344b5a
dywoq <dywoq.contact@gmail.com>
2026-05-17 12:33:29 +0300

    vqinstprint.h: document API
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- base/installer/inc/vqinstprint.h
+++ base/installer/inc/vqinstprint.h
@@ -14,26 +14,47 @@
 #include <vqdef.h>
 #include <vqinstval.h>
 
+/*
+ * Sets a background color, which applies to all printed characters.
+ */
 void
 PrintSetBgColor(VAL_COLOR color);
 
+/*
+ * Sets a foreground color, which is applied to all printed characters.
+ */
 void
 PrintSetFgColor(VAL_COLOR color);
 
+/*
+ * Provides the current set background color.
+ * The default one is VAL_BLACK.
+ */
 [[nodiscard]]
 VAL_COLOR
 PrintGetBgColor();
 
+/*
+ * Provides the current set foreground color.
+ * The default one is VAL_BRIGHT_WHITE.
+ */
 [[nodiscard]]
 VAL_COLOR
 PrintGetFgColor();
 
+/*
+ * Clears the whole screen, using the current set background color.
+ */
 static inline void
 PrintClearScreen()
 {
     ValClear(PrintGetBgColor());
 }
 
+/*
+ * Prints a string at fixed position (row, column), using the set foreground
+ * and background colors.
+ */
 void
 PrintStrFixed(
     const char *str,
