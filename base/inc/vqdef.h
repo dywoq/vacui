d212ab7e37a66eb7b4c3c87043f990cc8dff443e
dywoq <dywoq.contact@gmail.com>
2026-05-18 19:10:47 +0300

    vqdef.h: add
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ base/inc/vqdef.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2026 dywoq - Apache License 2.0
+ * https://github.com/dywoq/vacui
+ */
+
+#ifndef _VQDEF_H
+#define _VQDEF_H
+
+typedef char BYTE;
+typedef short SHORT;
+typedef int INT;
+typedef long long LONG;
+
+typedef unsigned char UBYTE;
+typedef unsigned short USHORT;
+typedef unsigned int UINT;
+typedef unsigned long long ULONG;
+
+#endif /* _VQDEF_H */
