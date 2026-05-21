d1503686997f1f6478ea0ab91e838ca981cece45
dywoq <dywoq.contact@gmail.com>
2026-05-21 09:18:36 +0300

    add vacui/types.h
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ inc/vacui/types.h
@@ -0,0 +1,16 @@
+// Copyright 2026 dywoq - Apache License 2.0
+// https://github.com/dywoq/vacui
+
+#ifndef _VACUI_TYPES_H
+#define _VACUI_TYPES_H
+
+typedef char vac_byte_t;
+typedef short vac_short_t;
+typedef int vac_int_t;
+typedef long long vac_long_t;
+typedef signed char vac_ubyte_t;
+typedef signed short vac_ushort_t;
+typedef signed int vac_uint_t;
+typedef signed long long vac_ulong_t;
+
+#endif
