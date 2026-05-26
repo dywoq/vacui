dd87b41c13c8b5a6c9d1830d84382b7406e14f06
dywoq <dywoq.contact@gmail.com>
2026-05-26 11:04:27 +0300

    hub.h: add hprint function
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- bal/i386/primary/hub/hub.c
+++ bal/i386/primary/hub/hub.c
@@ -1,6 +1,8 @@
 // Copyright 2026 dywoq - Apache License 2.0
 // https://github.com/dywoq/vacui
 
+#include <hub.h>
+
 __asm(".code16gcc");
 
 [[noreturn]] void hub()
---
+++ bal/i386/primary/inc/hub.h
@@ -0,0 +1,13 @@
+// Copyright 2026 dywoq - Apache License 2.0
+// https://github.com/dywoq/vacui
+//
+// Description:
+//      This module contains the hub functionality.
+#ifndef _HUB_H
+#define _HUB_H
+
+#include <vqtypes.h>
+
+void hprint(const byte_t *str);
+
+#endif
