f64567d5f1aa3290fdd8358818ab486569a32aaa
dywoq <dywoq.contact@gmail.com>
2026-05-26 21:44:02 +0300

    hub/hub.c: setup metadata in boot info
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- bal/i386/primary/hub/hub.c
+++ bal/i386/primary/hub/hub.c
@@ -1,12 +1,17 @@
 // Copyright 2026 dywoq - Apache License 2.0
 // https://github.com/dywoq/vacui
 
+__asm(".code16gcc");
+
+#include <balinfo.h>
 #include <hub.h>
 
-__asm(".code16gcc");
+struct bal_info *boot_info_ = {};
 
 [[noreturn]] void hub()
 {
+    boot_info_->metadata->name = "Vacui BAL i386";
+    boot_info_->metadata->license = "Apache License 2.0";
     while (1)
         __asm volatile("hlt\n");
 }
