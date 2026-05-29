a6c1cf20ea51a9aca1ca313061f5fa56bd552154
dywoq <dywoq.contact@gmail.com>
2026-05-29 15:24:13 +0300

    Remove balinfo.h header
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- bal/i386/primary/hub/hub.c
+++ bal/i386/primary/hub/hub.c
@@ -3,15 +3,10 @@
 
 __asm(".code16gcc");
 
-#include <balinfo.h>
 #include <hub.h>
-
-static struct bal_info *boot_info_ = {};
 
 [[noreturn]] void hub()
 {
-    boot_info_->metadata->name = "Vacui BAL i386";
-    boot_info_->metadata->license = "Apache License 2.0";
     while (1)
         __asm volatile("hlt\n");
 }
