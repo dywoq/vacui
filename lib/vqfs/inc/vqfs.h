c3859f9275138fd36dcc56f0dedd5cffdc7cb6b0
dywoq <dywoq.contact@gmail.com>
2026-05-29 12:55:36 +0300

    lib/vqfs: add vqfs.h header
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ lib/vqfs/inc/vqfs.h
@@ -0,0 +1,45 @@
+// Copyright 2026 dywoq - Apache License 2.0
+// https://github.com/dywoq/vacui
+//
+// Description:
+//
+//      Contains the definitions and functionality that relate to
+//      VQFS (VQ Filesystem).
+
+#ifndef _VQFS_H
+#define _VQFS_H
+
+#include <vqtypes.h>
+
+#define VQFS_HEADER_IS_CORRECT_SIGNATURE(signature)                            \
+    (signature[0] == 'V' && signature[1] == 'Q' && signature[2] == 'F' &&      \
+     signature[3] == 'S')
+
+struct [[gnu::packed]] vqfs_header {
+    char signature[4];
+    uint_t entry_offsets_offset;
+    char pad[4088];
+};
+
+#define VQFS_ENTRY_OFFSETS_MAX_SIZE 1024
+struct vqfs_entry_offsets {
+    ulong_t entries_offsets[VQFS_ENTRY_OFFSETS_MAX_SIZE];
+};
+
+#define VQFS_ENTRY_FLAG_READABLE 1 << 0
+#define VQFS_ENTRY_FLAG_WRITABLE 1 << 1
+#define VQFS_ENTRY_FLAG_SYSTEM 1 << 2
+#define VQFS_ENTRY_FLAG_HIDDEN 1 << 3
+#define VQFS_ENTRY_FLAG_DIR 1 << 4
+#define VQFS_ENTRY_FLAG_FILE 1 << 5
+#define VQFS_ENTRY_HAS_FLAG(num, flag) ((num & flag) != 0)
+struct [[gnu::packed]] vqfs_entry {
+    char filename[8];
+    char extension[3];
+    ubyte_t flags;
+    uint_t blocks_offsets[128];
+    uint_t file_size;
+    char pad[49];
+};
+
+#endif
