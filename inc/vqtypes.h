6466f445dccdc4f1bb56e1b4b4e7709352a863d0
dywoq <dywoq.contact@gmail.com>
2026-05-29 19:42:53 +0300

    vqtypes.h: add missing docs
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- inc/vqtypes.h
+++ inc/vqtypes.h
@@ -1,13 +1,24 @@
 // Copyright 2026 dywoq - Apache License 2.0
 // https://github.com/dywoq/vacui
+//
+// Description:
+//
+//      Shortcut type definitions over integral types
 
 #ifndef _VQTYPES_H
 #define _VQTYPES_H
 
+//
+// Signed types
+//
 typedef signed char byte_t;
 typedef signed short short_t;
 typedef signed int int_t;
 typedef signed long long long_t;
+
+//
+// Unsigned types
+//
 typedef unsigned char ubyte_t;
 typedef unsigned short ushort_t;
 typedef unsigned int uint_t;
