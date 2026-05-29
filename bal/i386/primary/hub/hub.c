cbff290967555b900dd3f409741814194d0815e8
dywoq <dywoq.contact@gmail.com>
2026-05-29 19:58:16 +0300

    bal/i386: hub/hub.c: load final bootloader stage into memory
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- bal/i386/primary/hub/hub.c
+++ bal/i386/primary/hub/hub.c
@@ -7,6 +7,7 @@
 
 __asm(".code16gcc");
 
+#include <bios.h>
 #include <gdt.h>
 #include <hub.h>
 
@@ -56,8 +57,36 @@ static void gdt_load_()
     __asm volatile("lgdt %0" : : "m"(gdt_) : "cc", "memory");
 }
 
+// Description:
+//
+//      Bootloader final stage DAP
+struct bios_dap final_stage_dap_ = {
+    .size = 16,
+    .reserved = 0,
+    .sectors_count = 1,
+    .offset = 0x1000,
+    .segment = 0x0,
+    .lba_start = 2
+};
+
+// Description:
+//
+//      Loads bootloader final stage into the memory, using bios_extended from
+//      bios.h.
+//
+//      Panics if carry flag is set after extended read operation, and does not
+//      return.
+static void final_stage_load()
+{
+    bool carry_flag;
+    bios_extended_read(&final_stage_dap_, 0x80, carry_flag);
+    if (carry_flag)
+        hpanic("Failed to load bootloader final stage");
+}
+
 [[noreturn]] void hub()
 {
+    final_stage_load();
     gdt_init_();
     gdt_load_();
     while (1)
