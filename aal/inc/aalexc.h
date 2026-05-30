111394dce7eb6c6afb1294bc8b0a5b90cc89213e
dywoq <dywoq.contact@gmail.com>
2026-05-30 11:05:45 +0300

    aal/inc: use /**/ comments
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- aal/inc/aalexc.h
+++ aal/inc/aalexc.h
@@ -1,9 +1,10 @@
-// Copyright 2026 dywoq - Apache License 2.0
-// https://github.com/dywoq/vacui
-//
-// Description:
-//
-//      Defines the AAL exception abstraction layer structs.
+/*
+Copyright 2026 dywoq - Apache License 2.0
+https://github.com/dywoq/vacui
+
+Description:
+    Defines the AAL exception abstraction layer structs.
+*/
 
 #ifndef _AALEXC_H
 #define _AALEXC_H
@@ -11,9 +12,10 @@
 #include <aaltier.h>
 #include <vqtypes.h>
 
-// Description:
-//
-//      Generic exception codes
+/*
+Description:
+    Generic exception codes
+*/
 typedef uint_t aal_exc_code_t;
 #define AAL_EXC_PAGE_FAULT          (aal_exc_code_t)0
 #define AAL_EXC_DATA_ACCESS_FAULT   (aal_exc_code_t)1
@@ -21,9 +23,10 @@ typedef uint_t aal_exc_code_t;
 #define AAL_EXC_SOFTWARE_BREAKPOINT (aal_exc_code_t)3
 #define AAL_EXC_ALIGNMENT_FAULT     (aal_exc_code_t)4
 
-// Description:
-//
-//      A generic exception information.
+/*
+Description:
+    A generic exception information.
+*/
 struct aal_exc_info
 {
     aal_exc_code_t what;
@@ -32,16 +35,18 @@ struct aal_exc_info
     aal_tier_t     previous_tier;
 };
 
-// Description:
-//
-//      A unified type alias for exception handler. It's passed a generic
-//      exception information.
+/*
+Description:
+    A unified type alias for exception handler. It's passed a generic
+    exception information.
+*/
 typedef void (*aal_exc_handler)(struct aal_exc_info *info);
 
-// Description:
-//
-//      An exception abstraction layer with function pointers,
-//      which allow you to set handler for various exceptions.
+/*
+Description:
+    An exception abstraction layer with function pointers,
+    which allow you to set handler for various exceptions.
+*/
 struct aal_exc_layer
 {
     void (*set_handler)(
