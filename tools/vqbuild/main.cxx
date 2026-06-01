14363d53c01ee1dea3190cf8731bc869dc2f199c
dywoq <dywoq.contact@gmail.com>
2026-06-01 12:46:59 +0300

    tools/vqbuild/main.cxx: automatically use folder to get config file
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- tools/vqbuild/main.cxx
+++ tools/vqbuild/main.cxx
@@ -11,13 +11,30 @@ Description:
 #include "vqbrun.h"
 #include <fstream>
 #include <iostream>
+#include <sstream>
 
-int main()
+int main(
+    int   argc,
+    char *argv[]
+)
 {
-    std::ifstream file("config");
+    if (argc == 1)
+    {
+        std::cerr << "Specify directory in arguments, where you need to build "
+                     "your target."
+                  << std::endl;
+        return 1;
+    }
+
+    const char *dir = argv[1];
+    std::stringstream config;
+    config << dir << "/" << "config";
+    std::string   config_str = config.str();
+    std::ifstream file(config_str.c_str());
     if (!file.is_open())
     {
-        std::cerr << "Failed to open \"config\" file" << std::endl;
+        std::cerr << "Failed to open \"" << config_str
+                  << "\" configuration file" << std::endl;
         return 1;
     }
 
@@ -25,7 +42,7 @@ int main()
     {
         vqbuild::config conf;
         conf.parse(file);
-        vqbuild::run(conf, "./");
+        vqbuild::run(conf, dir);
     }
     catch (const std::exception &e)
     {
