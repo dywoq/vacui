a4d9a4278f5d83be8ab4f35c186e15224168dbd8
dywoq <dywoq.contact@gmail.com>
2026-06-23 10:47:50 +0300

    private/tools/vqbuild: init folder
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ private/tools/vqbuild/startup/main.cxx
@@ -0,0 +1,20 @@
+/*
+    Copyright 2026 dywoq - Apache License 2.0
+    https://github.com/dywoq/vacui
+
+    Description:
+
+        Startup code of vqbuild
+
+*/
+
+#include <iostream>
+
+int
+main()
+{
+
+    std::cout << "Hi!" << std::endl;
+
+    return 0;
+}
