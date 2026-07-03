04d705d7b5046a4998e238966fce4fdaabaee0a7
dywoq <dywoq.contact@gmail.com>
2026-07-03 14:32:27 +0300

    private/tools: init vqbuild
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ private/tools/vqbuild/hub/main.c
@@ -0,0 +1,14 @@
+/*
+    Copyright 2026 dywoq - Apache License 2.0
+    https://github.com/dywoq/vacui
+
+    Description;
+
+        The vqbuild startup code
+*/
+
+int
+main()
+{
+    return 0;
+}
