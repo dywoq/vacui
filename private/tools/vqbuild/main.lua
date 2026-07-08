8ed9bf735134547e7c4ed4587cf0d574edadc75b
dywoq <dywoq.contact@gmail.com>
2026-07-08 10:17:45 +0300

    Reconstruction
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- private/tools/vqbuild/main.lua
+++
@@ -1,8 +0,0 @@
--- Copyright 2026 dywoq - Apache License 2.0
--- https://github.com/dywoq/vacui
-
-local conf = require "conf"
-local conf_table, err = conf.parse("./VQBUILD")
-if err ~= nil then 
-    error("Failed to parse config: " .. err)
-end
