d1d55d065373c81f0cb99d40c57a5b2f6ec45ad5
dywoq <dywoq.contact@gmail.com>
2026-08-23 13:58:23 +0300

    private/vqke: initialize a startup module
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ private/vqke/startup/init.c
@@ -0,0 +1,13 @@
+//
+// Copyright 2026 dywoq - Apache License 2.0
+// https://github.com/dywoq/vacui
+//
+// Routine Description
+//
+//      Kernel initialization routine
+//
+
+void
+StartupInitialize()
+{
+}
