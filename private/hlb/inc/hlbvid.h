af523a0afd154863471ff67879a8adfaa8c3b703
dywoq <dywoq.contact@gmail.com>
2026-08-23 16:03:13 +0300

    hlbvid.h: Declare video mode functions
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ private/hlb/inc/hlbvid.h
@@ -0,0 +1,54 @@
+//
+// Copyright 2026 dywoq - Apache License 2.0
+// https://github.com/dywoq/vacui
+//
+// Routine Description
+//
+//      HLB video services
+//
+
+#ifndef HLBVID_H
+#define HLBVID_H
+
+#include <vqdef.h>
+
+typedef VQ_USHORT HLB_VIDEO_MODE, *PHLB_VIDEO_MODE;
+
+//
+// Routine Description
+//
+//      Sets the current video mode to the provided one.
+//
+// Parameters
+//
+//      NewVideoMode
+//      - The new video mode. It must match the video modes specified in the
+//      documentation.
+//
+// BIOS Interrupt
+//
+//      AH = 0x00
+//      AL = NewVideoMode
+//
+void
+HlbSetVideoMode(HLB_VIDEO_MODE NewVideoMode);
+
+//
+// Routine Description
+//
+//      Gets the current video mode.
+//
+// Parameters
+//
+//      NewVideoMode
+//      - The new video mode. It must match the video modes specified in the
+//      documentation.
+//
+// BIOS Interrupt
+//
+//      AH = 0x0F
+//
+HLB_VIDEO_MODE
+HlbGetVideoMode();
+
+#endif
