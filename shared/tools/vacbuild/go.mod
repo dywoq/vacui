294a423fbc8039ea9d715878f3a4c77b29c13903
dywoq <dywoq.contact@gmail.com>
2026-08-23 17:30:37 +0300

    shared/tools/vacbuild: Implement module config parsing
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- shared/tools/vacbuild/go.mod
+++ shared/tools/vacbuild/go.mod
@@ -1,3 +1,5 @@
 module github.com/dywoq/vacui/shared/tools/vacbuild
 
 go 1.26.6
+
+require github.com/BurntSushi/toml v1.6.0 // indirect
---
+++ shared/tools/vacbuild/internal/config/module.go
@@ -0,0 +1,32 @@
+// Copyright 2026 dywoq - Apache License 2.0
+// https://github.com/dywoq/vacui
+
+package config
+
+import (
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+// Module contains the module configuration fields.
+type Module struct {
+	General struct {
+		TargetPath   string   `toml:"target_path"`
+		Sources      []string `toml:"sources"`
+		ObjsDir      string   `toml:"objs_dir"`
+		Dependencies []string `toml:"dependencies"`
+	} `toml:"general"`
+}
+
+// ParseModule reads the file and decodes it, returning a filled [Module].
+// The returned instance is filled only if the function decoded the file successfully.
+func ParseModule(file string) (*Module, error) {
+	bytes, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	var module *Module
+	_, err = toml.Decode(string(bytes), &module)
+	return module, err
+}
