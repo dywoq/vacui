07d9541185d8ba7792431e586877eca5f1350356
dywoq <dywoq.contact@gmail.com>
2026-08-23 14:19:14 +0300

    vqdef.h: Add integral type definitions
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ private/inc/vqdef.h
@@ -0,0 +1,24 @@
+//
+// Copyright 2026 dywoq - Apache License 2.0
+// https://github.com/dywoq/vacui
+//
+// Routine Description
+//
+//      Type, macro and constant definition, used across the Vacui
+//      components
+//
+
+#ifndef _VQDEF_H
+#define _VQDEF_H
+
+typedef signed char        VQ_BYTE;
+typedef signed short       VQ_SHORT;
+typedef signed int         VQ_INT;
+typedef signed long long   VQ_LONG;
+typedef unsigned char      VQ_UBYTE;
+typedef unsigned short     VQ_USHORT;
+typedef unsigned int       VQ_UINT;
+typedef unsigned long long VQ_ULONG;
+
+#endif
+
