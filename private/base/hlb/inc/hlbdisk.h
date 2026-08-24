16e3364ed2e82c52b6275d907261c31231749360
dywoq <dywoq.contact@gmail.com>
2026-08-24 22:00:11 +0300

    private/base/hlb: Initialize disk library
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ private/base/hlb/inc/hlbdisk.h
@@ -0,0 +1,13 @@
+//
+// Copyright 2026 dywoq - Apache License 2.0
+// https://github.com/dywoq/vacui
+//
+// Module Description
+//
+//      HLB Disk master header
+//
+
+#ifndef HLBDISK_H
+#define HLBDISK_H
+
+#endif
