ef7ade77375901e80dacb2a700ec85a32c658a9b
dywoq <dywoq.contact@gmail.com>
2026-08-24 12:17:59 +0300

    Create vacui.internal.def.h
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ private/inc/vacui.internal.def.h
@@ -0,0 +1,25 @@
+//
+// Copyright 2026 dywoq - Apache License 2.0
+// https://github.com/dywoq/vacui
+//
+// Routine Description
+//
+//      Type, macro and constant definition, used across the Vacui
+//      components
+//
+
+#ifndef _VACUI_INTERNAL_DEF_H
+#define _VACUI_INTERNAL_DEF_H
+
+typedef signed char        VQ_BYTE;
+typedef signed short       VQ_SHORT;
+typedef signed int         VQ_INT;
+typedef signed long long   VQ_LONG;
+typedef unsigned char      VQ_UBYTE;
+typedef unsigned short     VQ_USHORT;
+typedef unsigned int       VQ_UINT;
+typedef unsigned long long VQ_ULONG;
+typedef char               VQ_ASCIICHAR;
+
+#endif
+
