6b095af7c9fffb8ac476de97d3437cd0684e13e1
dywoq <dywoq.contact@gmail.com>
2026-08-24 15:57:21 +0300

    private/base/inc: add vqdef.h header
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ private/base/inc/vqdef.h
@@ -0,0 +1,28 @@
+//
+// Copyright 2026 dywoq - Apache License 2.0
+// https://github.com/dywoq/vacui
+//
+// Module Description
+//
+//      Type, macro and constant definitions used across the private
+//      Vacui components
+//
+
+#ifndef VQDEF_H
+#define VQDEF_H
+
+//
+// Integral types
+// 
+
+typedef signed char      BYTE;
+typedef signed short     SHORT;
+typedef signed int       INT;
+typedef signed long long LONG;
+
+typedef unsigned char      UBYTE;
+typedef unsigned short     USHORT;
+typedef unsigned int       UINT;
+typedef unsigned long long ULONG;
+
+#endif
