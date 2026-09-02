ecab08d044d4947825ec378cef8ff92189311191
dywoq <dywoq.contact@gmail.com>
2026-09-02 15:39:27 +0300

    base/fs/inc: Declare vqfs.h
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ base/fs/inc/vqfs.h
@@ -0,0 +1,18 @@
+//
+// Copyright 2026 dywoq - Apache License 2.0
+// https://github.com/dywoq/vacui
+//
+// Module Description
+//
+//      This header defines type, macro and structs of VQ Filesystem
+//      (or VQFS).
+//
+//      It is meant to be used within host operating system development tools
+//      and operating system components, therefore, the header defines its own
+//      integer types.
+//
+
+#ifndef VQFS_H
+#define VQFS_H
+
+#endif
