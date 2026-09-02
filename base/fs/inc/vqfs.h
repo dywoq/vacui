9d1ab401ce7cb21b410fe87396275bb038cfd2af
dywoq <dywoq.contact@gmail.com>
2026-09-02 16:57:20 +0300

    vqfs.h: Add documentation
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- base/fs/inc/vqfs.h
+++ base/fs/inc/vqfs.h
@@ -15,6 +15,10 @@
 #ifndef VQFS_H
 #define VQFS_H
 
+//
+// Integral types
+//
+
 typedef signed char        vqfs_byte_t;
 typedef signed short       vqfs_short_t;
 typedef signed int         vqfs_int_t;
@@ -24,11 +28,24 @@ typedef unsigned short     vqfs_ushort_t;
 typedef unsigned int       vqfs_uint_t;
 typedef unsigned long long vqfs_ulong_t;
 
+//
+// Constants
+//
+
 #define VQFS_BLOCK_SIZE         4096
 #define VQFS_BLOCKS_TOTAL_COUNT 524288
 #define VQFS_MAJOR_VERSION      1
 #define VQFS_MINOR_VERSION      0
 
+//
+// Routine Description
+//
+//      Header information of VQFS, which is at the very start of 
+//      a formatted drive. 
+// 
+//      It contains boot sector code, meta information and offsets to other 
+//      information data.
+//
 typedef struct vqfs_header {
     vqfs_ubyte_t boot_sector[512];
 
@@ -45,6 +62,12 @@ typedef struct vqfs_header {
     char padding[3568];
 } vqfs_header_t;
 
+//
+// Routine Description
+// 
+//      Table consisting of block offsets. It is placed after header 
+//      information.
+//
 typedef struct vqfs_blocks_table {
     vqfs_uint_t block_offsets[VQFS_BLOCKS_TOTAL_COUNT];
 } vqfs_blocks_table_t;
@@ -56,6 +79,13 @@ typedef vqfs_uint_t vqfs_time_t;
 #define VQFS_TIME_GET_MINUTE(time) (vqfs_uint_t)((time & (0xFF << 8)) >> 8)
 #define VQFS_TIME_GET_SECOND(time) (vqfs_uint_t)((time & (0xFF << 16)) >> 16)
 
+//
+// Routine Description
+// 
+//      Directory entry with a name, extension and flags, and meta 
+//      information, such as creation and last update time. If this is 
+//      a directory, then its block points to an array of directory entries.
+// 
 typedef struct vqfs_dir_entry {
     char        name[8];
     char        extension[4];
@@ -68,13 +98,24 @@ typedef struct vqfs_dir_entry {
 
 #define VQFS_DIR_ENTRIES_LIMIT VQFS_BLOCK_SIZE / sizeof(vqfs_dir_entry)
 
+//
+// Routine Description
+// 
+//      Root directory. It is placed after the blocks table.
+// 
 typedef struct vqfs_root_dir {
     vqfs_dir_entry_t dir_entries[VQFS_DIR_ENTRIES_LIMIT];
 } vqfs_root_dir_t;
 
+//
+// Routine Description
+// 
+//      Single block information. Next cluster index is pointing at a block 
+//      offset in the blocks table.
+// 
 typedef struct vqfs_block {
     char        content[4092];
-    vqfs_uint_t next_cluster_id;
+    vqfs_uint_t next_cluster_index;
 } vqfs_block_t;
 
 #endif
