fa8dcf4e143515c20335db2b8c790082b2e17ae1
dywoq <dywoq.contact@gmail.com>
2026-09-03 11:13:28 +0300

    base/bsl: Add integral types
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ base/bsl/inc/bsldef.h
@@ -0,0 +1,27 @@
+//
+// Copyright 2026 dywoq - Apache License 2.0
+// https://github.com/dywoq/vacui
+//
+// Module Description
+//
+//      BSL type and macro definitions
+//
+
+#ifndef BSLDEF_H
+#define BSLDEF_H
+
+//
+// Integral types
+//
+
+typedef signed char      bsl_byte_t;
+typedef signed short     bsl_short_t;
+typedef signed int       bsl_int_t;
+typedef signed long long bsl_long_t;
+
+typedef unsigned char      bsl_ubyte_t;
+typedef unsigned short     bsl_ushort_t;
+typedef unsigned int       bsl_uint_t;
+typedef unsigned long long bsl_ulong_t;
+
+#endif
