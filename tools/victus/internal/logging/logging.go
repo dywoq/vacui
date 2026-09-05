3db7681093cf1850eb2b364115edab772116f828
dywoq <dywoq.contact@gmail.com>
2026-09-05 08:36:56 +0300

    tools/victus: Add logging package
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ tools/victus/internal/logging/logging.go
@@ -0,0 +1,66 @@
+// Copyright 2026 dywoq - Apache License 2.0
+// https://github.com/dywoq/vacui
+
+package logging
+
+import (
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/fatih/color"
+)
+
+type Mode int
+
+const (
+	ModeEnabled Mode = iota
+	ModeDisabled
+)
+
+var (
+	modeMu sync.Mutex
+	mode   Mode = ModeDisabled
+)
+
+func SetMode(m Mode) {
+	modeMu.Lock()
+	defer modeMu.Unlock()
+	mode = m
+}
+
+func GetMode() Mode {
+	modeMu.Lock()
+	defer modeMu.Unlock()
+	return mode
+}
+
+func Errorf(format string, v ...any) {
+	modeMu.Lock()
+	defer modeMu.Unlock()
+	if mode == ModeDisabled {
+		return
+	}
+	prefix := color.RedString("ERROR: ")
+	fmt.Fprintf(os.Stderr, "%s: %s", prefix, fmt.Sprintf(format, v...))
+}
+
+func Infof(format string, v ...any) {
+	modeMu.Lock()
+	defer modeMu.Unlock()
+	if mode == ModeDisabled {
+		return
+	}
+	prefix := color.BlueString("INFO: ")
+	fmt.Fprintf(os.Stderr, "%s: %s", prefix, fmt.Sprintf(format, v...))
+}
+
+func Warnf(format string, v ...any) {
+	modeMu.Lock()
+	defer modeMu.Unlock()
+	if mode == ModeDisabled {
+		return
+	}
+	prefix := color.YellowString("WARN: ")
+	fmt.Fprintf(os.Stderr, "%s: %s", prefix, fmt.Sprintf(format, v...))
+}
--- tools/victus/readme.txt
+++
@@ -1,7 +0,0 @@
-_______________________________________________________________________________
-
-                                    Victus
-
-Victus - is a build orchestrator for Vacui operating system and user 
-components, used along with C/C++ development tools. It relies on GNU Make to 
-handle actual building. It supports TOML configuration files.
